15fe0846a21224815740092238e04a7dd891a845
tworrell <tara.worrell@gmail.com>
2020-02-11 20:20:06 -0500

    Added the changes to the counter to add the functionality

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+
+    //this is where we are going to put our methods
+    increment = () => {
+      this.setState ({
+        count: this.state.count + 1
+      })
+    }
+
+    decrement = () => {
+      this.setState ({
+        count: this.state.count - 1
+      })
+    }
+
+
+
+
+
+
+
+
+  render(){
+  return(
+    <div className ="container">
+      <div className = "navbar">Counter.js</div>
+      <div className = "counter">
+      <h1>{this.state.count}</h1>
+      <button type = "button" onClick={this.increment}>Increment</button>
+      <button type = "button" onClick={this.decrement}>Decrement</button>
+    </div>
+  </div>
+  );
+}
+}
+
+export default Counter;
