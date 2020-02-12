6d71770d5112661058622992d708057db2ea9a3a
tworrell <tara.worrell@gmail.com>
2020-02-12 00:43:23 -0500

    updated counter and tested. Toggle button is still WIP

--- src/App.js
+++ src/App.js
@@ -9,23 +9,40 @@ class Counter extends Component {
     };
   }
 
-
     //this is where we are going to put our methods
-    increment = () => {
-      this.setState ({
+    increment = () =>{
+    if(this.state.count >=20){
+      this.setState({
+        count:20
+      });
+    }else {
+      this.setState({
         count: this.state.count + 1
-      })
+      });
     }
+  };
 
-    decrement = () => {
-      this.setState ({
-        count: this.state.count - 1
-      })
+    decrement = () =>{
+    if(this.state.count< 1){
+      this.setState({
+        count:0
+      });
+    }else {
+      this.setState({
+        count: this.state.count- 1
+      });
     }
+  };
 
+    reset = () => {
+      this.setState({
+        count: 0
+    });
+  };
 
-
-
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
 
 
 
@@ -38,6 +55,11 @@ class Counter extends Component {
       <h1>{this.state.count}</h1>
       <button type = "button" onClick={this.increment}>Increment</button>
       <button type = "button" onClick={this.decrement}>Decrement</button>
+      <button type = "button" onClick ={this.reset}>Clear</button>
+      <button type = "button" onClick={this.ToggleClick}>
+          { this.state.show ? 'Double Increments' : 'Single Increments' }
+        </button>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
     </div>
   </div>
   );
